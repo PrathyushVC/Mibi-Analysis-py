a86f09b447f09ead6e0b73edb1afec5a6d4bc0da
PrathyushVC <chirra.prathyush@gmail.com>
2024-06-20 09:42:20 -0400

    added lazy loading to view images

--- ImageProcessing/make_hdf5.py
+++ ImageProcessing/make_hdf5.py
@@ -10,6 +10,7 @@ from numpy.lib.stride_tricks import as_strided  # as_strided allows for the crea
 from sklearn import model_selection
 import random
 import PIL
+import matplotlib.pyplot as plt
 
 
 def extract_patches(arr,patch_shape=32,stride=32):
@@ -32,7 +33,7 @@ def extract_patches(arr,patch_shape=32,stride=32):
 
 
 
-def make_hdf5(dataname,patch_size=32,stride_size=32,mirror_pad_size=16,test_set_size=0.1,resize=1,classes=np.arange(1),data_full_path=None,tables_base_path=None):
+def make_hdf5(dataname,patch_size=64,stride_size=64,mirror_pad_size=16,test_set_size=0.1,resize=1,classes=np.arange(1),data_full_path=None,tables_base_path=None):
     if data_full_path is None:
         raise ValueError("data_full_path must be provided")
     if tables_base_path is None:
@@ -97,7 +98,7 @@ def make_hdf5(dataname,patch_size=32,stride_size=32,mirror_pad_size=16,test_set_
                     logging.error(f"Error: {e}")
                     break#Just breaking to make it easy to debug
                 for imgtype in imgtypes:#This is basically a switch statement to handle the two image types.
-                    print(imgtype)
+                    
                     if(imgtype=="img"): #if we're looking at an img, it must be 3 channel, but cv2 won't load it in the correct channel order, so we need to fix that
                         io=Img_data['imageData']
                         io = np.repeat(io[:, :, np.newaxis], 3, axis=2)
@@ -133,18 +134,77 @@ def make_hdf5(dataname,patch_size=32,stride_size=32,mirror_pad_size=16,test_set_
                 
             npixels=hdf5_file.create_carray(hdf5_file.root, 'numpixels', tables.Atom.from_dtype(totals.dtype), totals.shape)
             npixels[:]=totals
-            hdf5_file.close()     
+            hdf5_file.close()  
+
+def binarize_segmentation(mask,class_to_keep=[]):
+    if class_to_keep is None:
+        raise Exception('class to keep must have at least one real integer value')
     
+
+
+def show_image(image_data,title='Image'):
+    plt.imshow(image_data, cmap='gray', vmin=0, vmax=25)
+    print(np.unique(image_data))
+    plt.title(title)
+    plt.axis('off')
+    plt.show(block=False)
+    plt.pause(5)
+    plt.close()
+
+def print_hdf5_file_data(file_path):
+    with tables.open_file(file_path, mode='r') as hdf:
+        # Print the structure of the HDF5 file
+        def print_structure(node, prefix=''):
+            print(f"{prefix}{node._v_name} ({type(node).__name__})")
+            if isinstance(node, tables.Group):
+                for child in node._v_children.values():
+                    print_structure(child, prefix + '  ')
+        print_structure(hdf.root)
+
+def lazy_load_imgs(hdf):
+    for img in hdf.root.img:
+        yield img
+
+def lazy_load_mask(hdf):
+    for mask in hdf.root.mask:
+        yield mask
+
+def test_samp_imgs(file_path,print_ximages=1000):
+    with tables.open_file(file_path, mode='r') as hdf:
+        images_generator = lazy_load_imgs(hdf)
+        try:
+            for i,image in enumerate(images_generator):
+                if i%print_ximages==0:
+                    first_image = next(images_generator)
+                    show_image(first_image, title='f{i}')
+        except StopIteration:
+            logging.info("no more images")
+        mask_gen = lazy_load_mask(hdf)
+        try:
+            for i,mask in enumerate(mask_gen):
+                if i%print_ximages==0:
+                    first_image = next(mask_gen)
+                    show_image(first_image, title='f{i}')
+        except StopIteration:
+            logging.info("no more images")
+
+
+        
  
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     current_dir = os.path.dirname(__file__)
+    print_ximages=1000
     #os.path.join(current_dir, r'..\..\..\Data_Full\**\*_CD4.npz')
     #os.path.join(current_dir, r'..\..\..\Scratch')
     #6,7,13 we are actually only interested in these 3 classes and everything else can be labeled background for this test
     #Setting it up like this makes it easy to sanitize later
     make_hdf5(dataname='Mibi_trial',data_full_path=os.path.join(current_dir, r'..\..\..\Data_Full\**\*_CD4.npz'),tables_base_path=os.path.join(current_dir, r'..\..\..\Scratch'))              
-                
+    #file_path='D:\MIBI-TOFF\Scratch\Mibi_trial_train.pytable'
+    #print_hdf5_file_data(file_path)
+    #test_samp_imgs(file_path,print_ximages=1000)
+
+    
 
 
 
