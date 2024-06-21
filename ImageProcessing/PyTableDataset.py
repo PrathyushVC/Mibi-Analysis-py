e130e6ee63b8d1cb9501781a56b4c57cfc6bad67
Mohsen Hariri <mhariri68@gmail.com>
2024-06-21 16:39:54 -0400

    put numpixels out of batches

--- ImageProcessing/PyTableDataset.py
+++ ImageProcessing/PyTableDataset.py
@@ -1,6 +1,6 @@
 import tables
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 
 
 class PyTableDataset(Dataset):
@@ -8,7 +8,7 @@ class PyTableDataset(Dataset):
         self.file = tables.open_file(pt_path, mode="r")
         self.images = self.file.root.img
         self.masks = self.file.root.mask
-        self.numpixels = self.file.root.numpixels
+        # self.numpixels = self.file.root.numpixels
 
     def __len__(self):
         return self.images.shape[0]
@@ -16,21 +16,29 @@ class PyTableDataset(Dataset):
     def __getitem__(self, idx):
         image = self.images[idx]
         mask = self.masks[idx]
-        numpixels = self.numpixels[idx]
+        # numpixels = self.numpixels[idx]
 
         image = torch.tensor(image, dtype=torch.float32)
         mask = torch.tensor(mask, dtype=torch.float32)
-        numpixels = torch.tensor(numpixels, dtype=torch.int32)
+        # numpixels = torch.tensor(numpixels, dtype=torch.int32)
 
-        return image, mask, numpixels
+        # return image, mask, numpixels
+        return image, mask
 
     def __del__(self):
         # https://teddykoker.com/2020/12/dataloader/
         self.file.close()
 
 
-if __name__ == "__main__":
-    pt_path = "Mibi_trial_val.pytable"
+def dataloader(pt_path, batch_size=64, shuffle=True):
     dataset = PyTableDataset(pt_path)
-    image, mask, numpixels = dataset[0]
-    print(image.shape, mask.shape, numpixels)
+    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+
+if __name__ == "__main__":
+
+    pt_path = "Mibi_trial_val.pytable"
+    dataloader = dataloader(pt_path)
+    for batch in dataloader:
+        images, masks = batch
+        print(images.shape, masks.shape)
+        break
